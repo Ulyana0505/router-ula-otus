a1093a4a6ea8913db80d67a309fe1567c787e164
Ulyana <lana.arfinova@gmail.com>
2024-02-20 16:41:49 +0300

    all tests done

--- tests/router.test.ts
+++ tests/router.test.ts
@@ -1,61 +1,92 @@
 import { routeTest, routePath, Router } from "../src/router";
 import { pageMain } from "../src/pages";
 
-describe('router', () => {
-    it('routeTest', () => {
-        // case "string"
-        expect(routeTest("rule", "2")).toEqual(false);
-        expect(routeTest("2", "2")).toEqual(true);
+describe("router", () => {
+  it('routeTest', () => {
+      // case "string"
+      expect(routeTest("rule", "2")).toEqual(false);
+      expect(routeTest("2", "2")).toEqual(true);
 
-        // case "function"
-        expect(routeTest((() => true), "2")).toEqual(true);
+      // case "function"
+      expect(routeTest((() => true), "2")).toEqual(true);
 
-        // default 
-        expect(routeTest(/e/, "2")).toEqual(false);
-        expect(routeTest(/2/, "2")).toEqual(true);
-    });
+      // default 
+      expect(routeTest(/e/, "2")).toEqual(false);
+      expect(routeTest(/2/, "2")).toEqual(true);
+  });
 
-    it('routePath', async() => {
-        expect(await routePath("current", {path: "routhPath"}, {route: null, path: "path"})).toEqual({ route: { path: 'routhPath' }, path: 'current' });
-    });
+  it('routePath', async() => {
+    expect(await routePath("current", {path: "routhPath"}, {route: null, path: "path"})).toEqual({ route: { path: 'routhPath' }, path: 'current' });
+});
 
-    it('Router', () => {
-        document.body.innerHTML = `
+  it('Router', () => {
+      document.body.innerHTML = `
+      <div class="menu">
+          <a href="/info">Info</a>
+          <a href="/path">Info</a>
+          <a href="/path-two">Info</a>
+      </div>
+      <div id="app"></div>`;
+      const router = Router();
+
+      router.on("/", pageMain);
+      expect(router.getRoutes().length).toEqual(1);
+
+      router.add({path: "/path"});
+      expect(router.getRoutes().length).toEqual(2);
+
+      router.remove({path: "/path"});
+      expect(router.getRoutes().length).toEqual(1);
+
+      // пробуем удалить несуществующй path
+      router.remove({path: "bad-route"});
+      expect(router.getRoutes().length).toEqual(1);
+
+      router.go(location.pathname);
+      expect(router.getRoutes().length).toEqual(1);
+
+      // test handlerClick
+      const a = document.querySelector("a") as HTMLAnchorElement;
+      a.click();
+      expect(a.getAttribute("href")).toEqual("/info");
+      // клик не по ссылке
+      a.parentElement?.click();
+      expect(a.parentElement!.getAttribute("href")).not.toEqual("/info");
+      // !href
+      a.removeAttribute("href");
+      a.click();
+      expect(a.getAttribute("href")).toEqual(null);
+  })
+
+  it("ROUTE", async () => {
+    document.body.innerHTML = `
         <div class="menu">
             <a href="/info">Info</a>
+            <a href="/path">Info</a>
+            <a href="/path-two">Info</a>
         </div>
         <div id="app"></div>`;
-        const router = Router();
 
-        router.on("/", pageMain);
-        expect(router.getRoutes().length).toEqual(1);
+    const onLeave = jest.fn();
+    const onBeforeEnter = jest.fn();
 
-        router.add({path: "path"});
-        expect(router.getRoutes().length).toEqual(2);
+    const router = Router();
+    router.add({ path: "/path-two", onLeave, onBeforeEnter });
+    router.on("/path", () => Promise.resolve());
 
-        router.remove({path: "path"});
-        expect(router.getRoutes().length).toEqual(1);
-        
-        // пробуем удалить несуществующй path
-        router.remove({path: "bad-route"});
-        expect(router.getRoutes().length).toEqual(1);
+    const pathTwo = document.querySelector(`[href="/path-two"]`) as HTMLElement;
+    pathTwo.click();
 
-        router.go(location.pathname);
-        expect(router.getRoutes().length).toEqual(1);
+    await wait();
 
-        // test handlerClick
-        const a = document.querySelector("a") as HTMLAnchorElement;
-        a.click();
-        expect(a.getAttribute("href")).toEqual("/info");
-        // клик не по ссылке
-        a.parentElement?.click();
-        expect(a.parentElement!.getAttribute("href")).not.toEqual("/info");
-        // !href
-        a.removeAttribute("href");
-        a.click();
-        console.log(a.getAttribute("href"));
-        expect(a.getAttribute("href")).toEqual(null);
+    const path = document.querySelector(`[href="/path"]`) as HTMLElement;
+    path.click();
 
+    expect(onBeforeEnter).toBeCalledTimes(1);
+    expect(onLeave).toBeCalledTimes(1);
+  });
+});
 
-    });
-})
+function wait() {
+  return new Promise((r) => setTimeout(r, 1));
+}
